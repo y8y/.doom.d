92356a73f7ac94b8139ce7543c52a3f900e83240
ChenBing <chenbing999@gmail.com>
2020-09-05 09:01:09 +0800

    kbd: add er/expand-region

--- kbd.el
+++ kbd.el
@@ -1,3 +1,8 @@
 ;;; ~/.doom.d/kbd.el -*- lexical-binding: t; -*-
 
 ;; (after! evil (map! "C-e" #'er/expand-region))
+(after! evil (map! :v "v"   (general-predicate-dispatch 'er/expand-region
+                              (eq (evil-visual-type) 'line)
+                              'evil-visual-char)
+                   :v "C-v" #'er/contract-region)
+  )
