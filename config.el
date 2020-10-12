7416e148ae014f20841dff7b72a9a06bd963dd9d
ChenBing <chenbing999@gmail.com>
2020-10-12 20:32:40 +0800

    fill-column => 120; 自动折行

--- config.el
+++ config.el
@@ -188,3 +188,7 @@
 (setq deft-directory "~/.deft"
       deft-extensions '("org" "txt" "md")
       deft-recursive t)
+
+;; 自动折行
+(add-hook 'org-mode-hook 'turn-on-auto-fill)
+(add-hook 'markdown-mode-hook 'turn-on-auto-fill)
