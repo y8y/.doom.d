cd5a1a73d8b76bbc70aee4e8eb149d9975016211
ChenBing <chenbing999@gmail.com>
2021-09-17 22:41:50 +0800

    feat: elegantpaper

--- config.el
+++ config.el
@@ -97,6 +97,28 @@
         ))
 ;; 设置默认后端为 `xelatex'
 (setq org-latex-compiler "xelatex")
+(with-eval-after-load 'ox-latex
+ ;; http://orgmode.org/worg/org-faq.html#using-xelatex-for-pdf-export
+ ;; latexmk runs pdflatex/xelatex (whatever is specified) multiple times
+ ;; automatically to resolve the cross-references.
+ (setq org-latex-pdf-process '("latexmk -xelatex -quiet -shell-escape -f %f"))
+ (add-to-list 'org-latex-classes
+               '("elegantpaper"
+                 "\\documentclass[lang=cn]{elegantpaper}
+                 [NO-DEFAULT-PACKAGES]
+                 [PACKAGES]
+                 [EXTRA]"
+                 ("\\section{%s}" . "\\section*{%s}")
+                 ("\\subsection{%s}" . "\\subsection*{%s}")
+                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                 ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+  (setq org-latex-listings 'minted)
+  (add-to-list 'org-latex-packages-alist '("" "minted")))
+(add-to-list 'image-type-file-name-regexps '("\\.pdf\\'" . imagemagick))
+(add-to-list 'image-file-name-extensions "pdf")
+(setq imagemagick-types-inhibit (remove 'PDF imagemagick-types-inhibit))
+(setq org-image-actual-width 600)
 
 ;; plantuml
 (setq plantuml-jar-path "~/soft/jar/plantuml.jar")
@@ -236,9 +258,9 @@
   (setq super-save-idle-duration 5))
 
 ;; disable company for org
-;; (setq company-global-modes '(not org-mode sh-mode))
-(setq company-global-modes '(not sh-mode))
+(setq company-global-modes '(not org-mode sh-mode))
 (setq company-minimum-prefix-length 3)
+(setq company-dabbrev-other-buffers nil)
 
 ;; jsx support
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . rjsx-mode))
